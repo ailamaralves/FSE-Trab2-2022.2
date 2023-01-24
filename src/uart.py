e8f8c217a63bae8efcf8474bcce976b5391083b0
ailamaralves <ailamar.alvesg@gmail.com>
2023-01-23 23:21:12 -0300

    fix uart list

--- src/uart.py
+++ src/uart.py
@@ -9,7 +9,7 @@ class UART:
     comando = [0x23, 0x16]
     #              0      1     2     3     4     5     6    7     8
     subComando = [0xC1, 0xC2, 0xC3, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6]
-    matricula = []
+    matricula:list
     def __init__(self, matricula):
         self.matricula = matricula
         self.conecta()
