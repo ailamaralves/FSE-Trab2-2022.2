0b2bd996f24dda8657f7a6e5b263775ae0432963
Ailamar Alves <ailamar.alvesg@gmail.com>
2023-01-24 01:51:05 -0300

    Update uart.py

--- src/uart.py
+++ src/uart.py
@@ -41,7 +41,7 @@ class UART:
             dado = struct.pack("f", dado)
         elif 0xD1 == self.subComando[subcomando]:
             dado = dado.to_bytes(4, 'little')
-        if subcomando in [0xD3, 0xD4, 0xD5]:
+        if self.subComando[subcomando] in [0xD3, 0xD4, 0xD5]:
             dado = [dado]
             mensagem = [self.endereco , self.comando[int(subcomando >= 4)] , self.subComando[subcomando]] + self.matricula + dado
             bmensagem = bytearray(mensagem)
